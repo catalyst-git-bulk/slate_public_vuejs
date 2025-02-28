088a1814484eef6ea93f5e8924ebd80c92f1de65
catalyst-git-test <nithyananthananthtest11@gmail.com>
2025-02-28 17:58:37 +0530

    Git Auto Deploy2

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home1</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/about">About1</RouterLink>
       </nav>
     </div>
   </header>
