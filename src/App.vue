4eb9f85f2498eae138261b455e8853221dfeb789
catalyst-git-test <nithyananthananthtest11@gmail.com>
2025-02-28 17:22:51 +0530

    Git Auto Deploy

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it! Deploy successful Test1" />
+      <HelloWorld msg="You did it!Deploy" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
